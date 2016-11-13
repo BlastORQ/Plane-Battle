dab114453221b069bc1618d3940e08fc8523c9f3
senjka4 <senjka4@gmail.com>
2016-11-13 17:20:04 +0200

    Створив проект
    створив проект

---
+++ desktop/src/ua/pp/blastorq/planebattle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ua.pp.blastorq.planebattle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ua.pp.blastorq.planebattle.PlaneBattle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlaneBattle(), config);
+	}
+}
