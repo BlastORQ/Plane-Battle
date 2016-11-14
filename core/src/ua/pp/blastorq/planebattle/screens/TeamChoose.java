ae6a99f22ecc41ebf0ea5e6349ba7ff7687b7f42
senjka4 <senjka4@gmail.com>
2016-11-14 18:34:45 +0200

    зробив вікно вибору команд

---
+++ core/src/ua/pp/blastorq/planebattle/screens/TeamChoose.java
@@ -0,0 +1,59 @@
+package ua.pp.blastorq.planebattle.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.*;
+
+public class TeamChoose implements Screen {
+    BitmapFont font;
+    SpriteBatch  batch;
+    Texture blue, red;
+    public TeamChoose() {
+        font = new BitmapFont(Gdx.files.internal("font/font.fnt") ,false);
+        batch = new SpriteBatch();
+        blue = new Texture("blue.png");
+    }
+
+    @Override
+    public void show() {
+   }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        font.setColor(Color.BLACK);
+        font.draw(batch, "Виберіть команду:",(Gdx.graphics.getWidth()/2) -200 , (Gdx.graphics.getHeight()/2) + 150);
+        batch.draw(blue,(Gdx.graphics.getWidth()/2) -200 , (Gdx.graphics.getHeight()/2) + 100);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
