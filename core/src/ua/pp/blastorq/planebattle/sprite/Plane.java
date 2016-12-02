28d6a90f2ec0b940ac3c2bb80c4a97e76f2416cd
senjka4 <senjka4@users.noreply.github.com>
2016-12-02 21:31:11 +0200

    Синхронізація літачків

--- core/src/ua/pp/blastorq/planebattle/sprite/Plane.java
+++ core/src/ua/pp/blastorq/planebattle/sprite/Plane.java
@@ -3,12 +3,22 @@ package ua.pp.blastorq.planebattle.sprite;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 
 public class Plane extends Sprite {
+    Vector2 previousPosition;
     public Plane(Texture texture) {
         super(texture);
-
-        setPosition((Gdx.graphics.getWidth() /2) - (getWidth()/2), 128);
-
+        previousPosition = new Vector2(getX(), getY());
+        setPosition((Gdx.graphics.getWidth() /2) - (getWidth()/2), 64);
+    }
+    public boolean hasMoved(){
+        if(previousPosition.x != getX() || previousPosition.y != getY()){
+            previousPosition.x = getX();
+            previousPosition.y = getY();
+            return true;
+        }
+        return false;
     }
 }
+
