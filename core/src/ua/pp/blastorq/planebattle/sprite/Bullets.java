53d7b74227a4622fb5035e5ec93f91a779fbd739
Manzhula David <da411d@gmail.com>
2017-02-17 23:00:46 +0200

    Додав обробку зіткнень

--- core/src/ua/pp/blastorq/planebattle/sprite/Bullets.java
+++ core/src/ua/pp/blastorq/planebattle/sprite/Bullets.java
@@ -2,10 +2,12 @@ package ua.pp.blastorq.planebattle.sprite;
 
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
 import ua.pp.blastorq.planebattle.loader.data;
+import ua.pp.blastorq.planebattle.screens.MainMenuScreen;
 
 public class Bullets extends Array<Bullet> {
     public long lastAddPlayer, lastAddBot;
@@ -13,6 +15,18 @@ public class Bullets extends Array<Bullet> {
         for (Bullet bullet : this) {
             data.batch.draw(data.getBulletTexture(), bullet.x, bullet.y, 64, 64);
             bullet.calculate(Gdx.graphics.getDeltaTime());
+            if(Intersector.overlaps(data.player.getBoundingRectangle(), bullet)){
+                this.onOverlap(false);
+            }else if(Intersector.overlaps(data.bot.getBoundingRectangle(), bullet)){
+                this.onOverlap(true);
+            }
+        }
+    }
+    void onOverlap(boolean isBot){
+        if(isBot){
+            data.bot.shot();
+        }else{
+            data.player.shot();
         }
     }
     void append(Bullet b, boolean bot){
